1b6cc4f184be500028668fa20daea4d206964a9f
Gedielson Vieira <gedielsonvieira@outlook.com>
2021-04-18 13:32:25 -0300

    Tentando implementar Modificadores de acesso

--- Atividade3/Veiculo.py
+++ Atividade3/Veiculo.py
@@ -28,17 +28,3 @@ class Veiculo():
 
     def frear(self, velocidadeAtual):
         self.__velocidade = self.__velocidade - velocidadeAtual
-
-'''Quando se utilizar modificadores se acesso: '''
-# Cria-se método set para cada atributo que se quer permitir que seja modificado fora da própia classe
-
-'''
-No diagrama de classe:
-O sinal de - é privado (__) -> só pode ser modificado e acessado dentro da sua própia classe
-O sinal de # é protegido (_) -> só pode ser acessado e modificado dentro da própria classe e nas classes que herdam destas classes e classes dentro do mesmo pacote
-O sinal de + é publico -> pode ser acessado e modificado dentro de qualquer classe do projeto
-'''
-
-#pacote é um diretório que dentro deste diretório tem um conjunto de classes 
-#Pacote é um conjunto de classes que estão no mesmo diretório
-#Pacote é um diretório -> dentro do diretório tem arquivos -> dentro de arquivos tem as classes que por sua vez tem a estrutura de dados.
